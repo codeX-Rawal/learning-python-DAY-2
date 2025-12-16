9ba6a046822e4e14eb8282816bd1acb01f298f3b
Rawal Singh <rawalsinghr798@gmail.com>
2025-12-16 10:48:35 +0530

    Add files via upload
    DAY-2: Learned Variables and basic data type.

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Day 2: Variables in Python
+
+# Integer variable
+age = 18
+print("Age:", age)
+
+# Float variable
+height = 5.8
+print("Height:", height)
+
+# String variable
+name = "Rawal"
+print("Name:", name)
+
+# Boolean variable
+is_student = True
+print("Is Student:", is_student)
+
+# Taking input from user
+city = input("Enter your city: ")
+print("City:", city)
+
+# Updating variable
+age = age + 1
+print("Age after one year:", age)
